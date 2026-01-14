6114421b34396e5027176e45df41e572ab90dc3c
Шарипов Тимур Махкамович <t.sharipov@sorb-group.loc>
2026-01-14 16:22:32 +0300

    fix ./internal/mq

--- internal/mq/producer.go
+++ internal/mq/producer.go
@@ -3,20 +3,10 @@ package mq
 import (
 	"context"
 	"log"
-	"sync/atomic"
 	"time"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
-
-type Mq struct {
-	Conn        *amqp.Connection
-	Channel     *amqp.Channel
-	Queue       string
-	retryBuffer chan []byte
-	confirms    <-chan amqp.Confirmation
-	connected   atomic.Bool
-}
 
 func NewMq(ctx context.Context, url, queue string) *Mq {
 	var conn *amqp.Connection
@@ -81,76 +71,6 @@ func NewMq(ctx context.Context, url, queue string) *Mq {
 			go mq.retryLoop(ctx)
 
 			return mq
-		}
-	}
-}
-
-func (m *Mq) Publish(payload []byte) error {
-	err := m.Channel.Publish(
-		"",
-		m.Queue,
-		false,
-		false,
-		amqp.Publishing{
-			ContentType: "application/json",
-			Body:        payload,
-		},
-	)
-	if err != nil {
-		log.Printf("Publish error, pushing to retryBuffer: %v", err)
-
-		m.connected.Store(false)
-
-		select {
-		case m.retryBuffer <- payload:
-		default:
-			log.Println("retryBuffer full, dropping message")
-		}
-
-		return err
-	}
-
-	return nil
-}
-
-func (m *Mq) retryLoop(ctx context.Context) {
-	for {
-		select {
-		case <-ctx.Done():
-			log.Println("retryLoop stopped")
-			return
-		default:
-			if !m.connected.Load() {
-				time.Sleep(500 * time.Millisecond)
-				continue
-			}
-
-			select {
-			case msg := <-m.retryBuffer:
-				err := m.Channel.Publish(
-					"",
-					m.Queue,
-					false,
-					false,
-					amqp.Publishing{
-						ContentType: "application/json",
-						Body:        msg,
-					},
-				)
-				if err != nil {
-					log.Println("retry publish failed:", err)
-					m.connected.Store(false)
-
-					time.Sleep(2 * time.Second)
-					select {
-					case m.retryBuffer <- msg:
-					default:
-						log.Println("retryBuffer full, dropping message")
-					}
-				}
-			default:
-				time.Sleep(200 * time.Millisecond)
-			}
 		}
 	}
 }
