ed834451477fdb62d749875598c3a58d441e68d8
Шарипов Тимур Махкамович <t.sharipov@sorb-group>
2026-02-11 15:09:42 +0300

    fixe pg and connection

--- internal/db/mongoDb/connection.go
+++ internal/db/mongoDb/connection.go
@@ -10,24 +10,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var MongoClient *mongo.Client
-
-func Init(ctx context.Context, uri string) {
-	var err error
-	MongoClient, err = Connect(ctx, uri)
-	if err != nil {
-		log.Fatalf("failed to initialize mongo: %v", err)
-	}
-}
-
-func Close(ctx context.Context) {
-	if MongoClient != nil {
-		if err := MongoClient.Disconnect(ctx); err != nil {
-			log.Printf("error closing mongo connection: %v", err)
-		}
-	}
-}
-
 func Connect(ctx context.Context, uri string) (*mongo.Client, error) {
 	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
 	defer cancel()
